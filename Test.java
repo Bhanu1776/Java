25ce51add45a6ef17f4b9a121ff5a2fdec312d12
Bhanu <sunkabhanu281202@gmail.com>
2021-08-27 02:24:52 +0530

    Basic Syntax

---
+++ Test.java
@@ -0,0 +1,25 @@
+// public class Test       //* College 
+// {
+//     public static void name(String[] args ) 
+//     {
+//         System.out.println("Hello World");
+//     }   
+// }
+
+// class DemoFile          //* Random Online 
+// {  
+// public static void main(String args[])  
+// {  
+// System.out.println("Hello!");  
+// System.out.println("Java");
+// }  
+// }  
+
+public class Test {      //* Harry 
+
+    public static void main (String[] args){
+        String b = "Test";
+        System.out.println(b);          //* Red dot to breakpoint debuging 
+    }
+}
+
