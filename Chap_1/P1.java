692226c6457a72cf5464348e987025ea65a677e5
Bhanu <sunkabhanu281202@gmail.com>
2021-09-02 16:20:35 +0530

    Sum_three_num

---
+++ Chap_1/P1.java
@@ -0,0 +1,12 @@
+//* Write a program to sum three numbers in Java.
+
+public class P1 {
+    public static void main(String[] args) {
+        
+        int a = 6;
+        int b = 17;
+        int c = 63;
+    
+        System.out.println("The sum of three numbers are = "+ (a+b+c) );
+    }
+}
