e5f10ae4271c7cdce6e767dba38ba43e1ba552d4
Bhanu <sunkabhanu281202@gmail.com>
2021-11-04 18:31:18 +0530

    Arrays in java

---
+++ Tpo Course/src/Array.java
@@ -0,0 +1,23 @@
+package src;
+import java.util.*;
+
+public class Array{
+    public static void main(String[] args) {
+        
+        int[] arr= new int[5];          //* arr is a variable where it can store upto 5 int values .. //! General syntax of declaring an array
+
+        System.out.print("Enter 5 inputs : ");
+        Scanner a = new Scanner(System.in);
+
+        for(int i=0 ; i<=4; i++){           //* i is staring from 0 coz in arrays index no is from 0 nor 1 ..
+
+            arr[i]= a.nextInt();            //* taking input in array ..
+        }
+
+        for(int i=0 ; i<=4; i++){          
+
+            System.out.print(arr[i]+" ");
+        }
+        
+    }
+}
