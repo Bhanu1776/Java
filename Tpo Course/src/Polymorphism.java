2269cda14c03f50c2ed3826a16d098ecd71dafa7
Bhanu <sunkabhanu281202@gmail.com>
2021-11-04 18:35:32 +0530

    Polymorphism : Method Overloading in java

---
+++ Tpo Course/src/Polymorphism.java
@@ -0,0 +1,26 @@
+package src;
+
+//* Polymorphism : One object which exist in many forms is called polymorphism
+
+public class Polymorphism {
+
+    public static void main(String[] args) {
+        
+        Overloading obj = new Overloading();
+        obj.m1(10);
+        obj.m1("bhanu");
+    }
+}
+
+    class Overloading{
+
+       
+//*     1) Method Overloading : If no.of parameters, sequence of paremeters and datatype of parameters are different in same class then Method overloading exists ..
+
+        public void m1(int x){                    // Here both methods having same name but different parameters thats why it comes under MethodOverloading 
+            System.out.println("Integer is passed");
+        }
+        public void m1(String str){
+            System.out.println("String is passed");
+        }
+    }
